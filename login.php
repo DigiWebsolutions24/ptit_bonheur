8023e9f711810169a12347687d7e4258a62d260f
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 23:52:14 +0200

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if (isset($_POST['username'])){
 	$username = mysqli_real_escape_string($conn, $username);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($conn, $password);
-    $query = "SELECT * FROM `users` INNER JOIN `enfants` WHERE username='$username' and password='".hash('sha256', $password)."'";
+    $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
 	$result = mysqli_query($conn,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
 	if($rows==1){
@@ -27,7 +27,7 @@ if (isset($_POST['username'])){
 <body>
 
 <form class="box" action="" method="post" name="login">
-<h1 class="box-logo box-title">Bienvenue à la MAM Les P'tit Bonheur !</h1>
+<h1 class="box-logo box-title">Bienvenue à la MAM Les P'tit Bonheur !</a></h1>
 <h1 class="box-title">Connexion</h1>
 <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
 <input type="password" class="box-input" name="password" placeholder="Mot de passe">
