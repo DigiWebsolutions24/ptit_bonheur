9451e9a426ba489e915ff5d6188f5155405706c5
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 22:51:57 +0200

    Fin mode de connexion

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<body>
 		<div class="sucess">
 		<h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
-		<p>C'est votre tableau de bord.</p>
+		<p>Bienvenue sur votre tableau de bord</p>
 		<a href="logout.php">Déconnexion</a>
 		</div>
 	</body>
--- login.php
+++ login.php
@@ -27,12 +27,11 @@ if (isset($_POST['username'])){
 <body>
 
 <form class="box" action="" method="post" name="login">
-<h1 class="box-logo box-title"><a href="https://waytolearnx.com/">WayToLearnX.com</a></h1>
+<h1 class="box-logo box-title">Bienvenue à la MAM Les P'tit Bonheur !</h1>
 <h1 class="box-title">Connexion</h1>
 <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
 <input type="password" class="box-input" name="password" placeholder="Mot de passe">
 <input type="submit" value="Connexion " name="submit" class="box-button">
-<p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
 <?php if (! empty($message)) { ?>
     <p class="errorMessage"><?php echo $message; ?></p>
 <?php } ?>
