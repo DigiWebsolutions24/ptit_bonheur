534c42a6b63a35fbe0e79a9c84975d0beb973371
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 21:30:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les P'tit Bonheur</title>
     <link rel="stylesheet" href="./env/style.css">
+
+</head>
+
+<body>
     <?php 
     include ("./env/connect.php");
     include ("./env/connexion.php");
 
     ?>
-</head>
-
-<body>
-
     <section class="page_connexion">
 
         <h1>Page de connexion</h1>
