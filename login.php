aad27232f5b31faf41c7c93db4f10cf71a22ff86
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 23:54:51 +0200

    Update login.php

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if (isset($_POST['username'])){
 <body>
 
 <form class="box" action="" method="post" name="login">
-<h1 class="box-logo box-title">Bienvenue à la MAM Les P'tit Bonheur !</a></h1>
+<h1 class="box-logo box-title">Bienvenue à la MAM Les P'tit Bonheur !</h1>
 <h1 class="box-title">Connexion</h1>
 <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
 <input type="password" class="box-input" name="password" placeholder="Mot de passe">
