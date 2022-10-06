b13ecb8f541762861b06ea91705f224e390bd019
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 21:51:20 +0200

    Update connexion.php

--- env/connexion.php
+++ env/connexion.php
@@ -1,14 +1,14 @@
 <?php
 include('./env/connect.php');
 session_start();
-if (isset($_POST['username'])){
-  $username = stripslashes($_REQUEST['username']);
+if (isset($_POST['NOM'])){
+  $username = stripslashes($_REQUEST['NOM']);
   $username = mysqli_real_escape_string($conn, $username);
-    $query = "SELECT * FROM `users` WHERE username='$username'";
+    $query = "SELECT * FROM `users` WHERE NOM='$username'";
   $result = mysqli_query($conn,$query) or die(mysql_error());
   $rows = mysqli_num_rows($result);
   if($rows==1){
-      $_SESSION['username'] = $username;
+      $_SESSION['NOM'] = $username;
       header("Location: index.php");
   }else{
     $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
