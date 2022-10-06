d2ab61068cd124eeb406384b0230caa48379676e
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 19:29:36 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,6 +9,4 @@ $db = new PDO("mysql:host={$host};dbname={$dbname}", $user, $pass, $pdo_option);
 $pdo_option->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 $liste_enfants = $db->query('SELECT * FROM enfants');
-                
-echo $db;
 >
