fec710c7b3409ac9a29aeac1b718cb71aa2a1c81
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 21:46:37 +0200

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les P'tit Bonheur</title>
-    <link rel="stylesheet" href="./env/style.css">
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
