e524a0edf1c192bf1666c8c9a457b2649df2060f
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 23:35:55 +0200

    Update login.php

--- login.php
+++ login.php
@@ -8,10 +8,8 @@ if (isset($_POST['username'])){
 	$username = mysqli_real_escape_string($conn, $username);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($conn, $password);
-    $queryUser = "SELECT * FROM `users`
-	INTERSECT
-	SELECT * FROM `enfants` WHERE username='$username' and password='".hash('sha256', $password)."'";
-	$result = mysqli_query($conn,$queryUser) or die(mysql_error());
+    $query = "SELECT * FROM `users` INTERSECT SELECT * FROM `enfants` WHERE username='$username' and password='".hash('sha256', $password)."'";
+	$result = mysqli_query($conn,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
 	if($rows==1){
 	    $_SESSION['username'] = $username;
