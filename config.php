2a6b89339df66136cdf23cc103e6a7c35476c904
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 22:07:25 +0200

    test

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 // Informations d'identification
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'registration');
+define('DB_SERVER', 'digimyshjhweb.mysql.db');
+define('DB_USERNAME', 'digimyshjhweb');
+define('DB_PASSWORD', 'Digi24Websolutions150');
+define('DB_NAME', 'digimyshjhweb');
  
-// Connexion à la base de données MySQL 
+// Connexion ï¿½ la base de donnï¿½es MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// Vérifier la connexion
+// Vï¿½rifier la connexion
 if($conn === false){
     die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
 }
