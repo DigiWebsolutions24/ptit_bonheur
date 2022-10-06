5afb16d175cf4e98439d5bfdbcf61056b1bfa426
Fernandlabarre <labarre.f24@gmail.com>
2022-10-06 21:57:06 +0200

    Update connexion.php

--- env/connexion.php
+++ env/connexion.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_POST['NOM'])){
   $username = stripslashes($_REQUEST['NOM']);
   $username = mysqli_real_escape_string($conn, $username);
-    $query = "SELECT * FROM `users` WHERE NOM='$username'";
+    $query = "SELECT * FROM `enfants` WHERE NOM='$username'";
   $result = mysqli_query($conn,$query) or die(mysql_error());
   $rows = mysqli_num_rows($result);
   if($rows==1){
